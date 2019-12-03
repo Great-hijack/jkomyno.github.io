a4f037c49b7c8f5ad2ab81f5f507a8e9fba9308d
Travis CI User <travis@example.org>
2019-12-03 22:19:52 +0000

    Travis build

---
+++ precache-manifest.615cd225ad0a88b6e9c9447567a76178.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad9ccc5940cadb523e7b3c6d16940bc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5848dd232393b0f34843",
+    "url": "/static/css/main.b353f7e3.chunk.css"
+  },
+  {
+    "revision": "85afda84a72ba4101e3f",
+    "url": "/static/js/2.0634e65e.chunk.js"
+  },
+  {
+    "revision": "5848dd232393b0f34843",
+    "url": "/static/js/main.b56b94f8.chunk.js"
+  },
+  {
+    "revision": "e0ff34aac396c6a5e6f6",
+    "url": "/static/js/runtime-main.2957677c.js"
+  }
+]);
